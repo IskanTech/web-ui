0cc251a6af270cd6d568ff474b610e2d17644013
esyywar <rahuleswar@gmail.com>
2024-09-18 23:05:59 -0700

    placeholder footer added in main layout

---
+++ components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="absolute bottom-0 w-full bg-mainViolet shadow dark:bg-gray-800">
+        <div className="w-full max-w-screen-xl m-auto p-4 md:flex md:items-center md:justify-between text-white">
+            <h1>Logo Placeholder</h1>
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0">
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mainViolet': '#382F9C' 
+      },
+    },
   },
   plugins: [],
 }
